a086b1136e0ac17ab357827ea1653359ba0b4f0c
Víctor Gabriel Moreno <victorgmp@gmail.com>
2019-08-30 18:06:48 -0400

    add new view

--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -2,12 +2,14 @@ import { Schema, model, Document } from 'mongoose';
 
 export interface IUser extends Document {
   username: string;
+  password?: string;
   googleId: string;
   thumbnail: string;
 }
 
 const userSchema = new Schema({
   username: { type: String },
+  password: { type: String },
   googleId: { type: String },
   thumbnail: { type: String },
 });
